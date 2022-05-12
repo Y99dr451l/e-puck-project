9a205a8b6aa66ec896e13440363dcd0da306162b
Y99dr451l <matthias@kockisch.eu>
2022-05-12 22:00:10 +0200

    + time-of-flight sensor

--- bot.c
+++ bot.c
@@ -11,14 +11,13 @@
 #include "communications.h"
 #include "pi_regulator.h"
 
-#define MM_PER_STEP 0.12566f
-#define WHEEL_D 4
+#define MM_PER_STEP (0.12566f*0.322f)
 #define INV_ROBOT_WIDTH 0.0185185185f // 1/54
+#define TOF_DIST 100 // round-trip, so 50mm in real distance
 
 static float position[3] = {0., 0., 0.}; // mm, mm, rad
 static float destination[3] = {100., 0., 0.}; // mm, mm, rad
 static int16_t* speeds;
-static void bot_reset(void);
 
 static THD_WORKING_AREA(waRunBot, 512);
 static THD_FUNCTION(RunBot, arg) { // higher priority for no PI interruptions
@@ -38,7 +37,7 @@ static THD_FUNCTION(RunBot, arg) { // higher priority for no PI interruptions
 //				uint16_t size = ReceiveFloatFromComputer((BaseSequentialStream *) &SD3, destination, 3);
 //				if (size == 3) break;
 //			}
-			first_start = false; null_speed_counter = 0; clear_leds(); bot_reset();
+			first_start = false; null_speed_counter = 0; clear_leds();
 			GPTD12.tim->CNT = 0; // time correction for odometry
 		}
 		set_body_led(1);
@@ -53,19 +52,26 @@ static THD_FUNCTION(Odometry, arg) {
     float dleft, dright, dcenter, phi;
     while(1) {
     	time = chVTGetSystemTime();
-    	speeds = get_speeds();
+    	if (VL53L0X_get_dist_mm() < TOF_DIST) {
+    		left_motor_set_speed(0); right_motor_set_speed(0);
+    		for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 10, 10); // cyan
+    		GPTD12.tim->CNT = 0;
+    		continue;
+    	} else for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 0, 0);
     	chSysLock(); // time-sensitive calculations
     	uint16_t timestep = GPTD12.tim->CNT; // /!\ max time of ~650ms
 		if (speeds[0] != 0 && speeds[1] != 0) { // http://faculty.salina.k-state.edu/tim/robotics_sg/Control/kinematics/odometry.html
-			dleft =  WHEEL_D*speeds[0]*timestep*1e-4; // timestep is in 10us
-			dright = WHEEL_D*speeds[1]*timestep*1e-4;
+			dleft =  MM_PER_STEP*speeds[0]*timestep*1e-5; // timestep is in 10us
+			dright = MM_PER_STEP*speeds[1]*timestep*1e-5;
 			dcenter = 0.5*(dleft+dright);
 			phi = INV_ROBOT_WIDTH*(dright-dleft);
-			position[X_] += dcenter*cos(position[T_]+0.5*phi); // front at heading 0
-			position[Y_] += dcenter*sin(position[T_]+0.5*phi); // left at heading 0
+			position[X_] += dcenter*cos(position[T_]+0.5*phi);
+			position[Y_] += dcenter*sin(position[T_]+0.5*phi);
 			position[T_] += phi;
 		}
 		chSysUnlock();
+		speeds = get_speeds();
+		left_motor_set_speed(speeds[0]); right_motor_set_speed(speeds[1]);
 		GPTD12.tim->CNT = 0;
 		chThdSleepUntilWindowed(time, time + MS2ST(10)); // 100Hz
     }
@@ -75,11 +81,6 @@ float* get_position(void) {return position;}
 float* get_destination(void) {return destination;}
 
 void bot_start(void) {
-	chThdCreateStatic(waOdometry, sizeof(waOdometry), NORMALPRIO, Odometry, NULL);
 	chThdCreateStatic(waRunBot, sizeof(waRunBot), NORMALPRIO+1, RunBot, NULL);
-}
-
-void bot_reset(void) {
-	// speeds to 0
-	// error_sum to 0
+	chThdCreateStatic(waOdometry, sizeof(waOdometry), NORMALPRIO, Odometry, NULL);
 }
