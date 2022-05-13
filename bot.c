f4277c7ee3786f28ebfe0c683a2ed0b9ae5876ac
Y99dr451l <matthias@kockisch.eu>
2022-05-14 01:38:35 +0200

    correction factors and receive destination

--- bot.c
+++ bot.c
@@ -6,37 +6,37 @@
 #include <math.h>
 #include "button.h"
 #include "leds.h"
+#include "sensors/VL53L0X/VL53L0X.h"
 #include "main.h"
 #include "bot.h"
 #include "communications.h"
 #include "pi_regulator.h"
 
-#define MM_PER_STEP (0.12566f*0.322f)
-#define INV_ROBOT_WIDTH 0.0185185185f // 1/54
-#define TOF_DIST 100 // round-trip, so 50mm in real distance
+#define MM_PER_STEP (0.12566f*0.322f) // actual value * weird correction
+#define INV_ROBOT_WIDTH (0.01852f * 2) // 1/54, same thing here
+#define TOF_DIST 100 // round-trip, so ~50mm in real distance
 
 static float position[3] = {0., 0., 0.}; // mm, mm, rad
-static float destination[3] = {100., 0., 0.}; // mm, mm, rad
+static float destination[3] = {100., 100., 0.}; // mm, mm, rad
 static int16_t* speeds;
 
 static THD_WORKING_AREA(waRunBot, 512);
 static THD_FUNCTION(RunBot, arg) { // higher priority for no PI interruptions
     chRegSetThreadName(__FUNCTION__); (void)arg;
-    bool first_start = false;
+    bool first_start = true;
     uint8_t null_speed_counter = 0;
     systime_t time;
     while(1) {
     	time = chVTGetSystemTime();
     	speeds = get_speeds();
 		if (speeds[0] == 0 && speeds[1] == 0) null_speed_counter++;
-		if(button_is_pressed() || first_start || null_speed_counter > 20) {
-			clear_leds();
+		if(button_is_pressed() || null_speed_counter > 20 || first_start) {
+			clear_leds(); set_body_led(0); set_front_led(0);
 			left_motor_set_speed(0); right_motor_set_speed(0);
-			for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 10, 10); // cyan
-//			while(1) {
-//				uint16_t size = ReceiveFloatFromComputer((BaseSequentialStream *) &SD3, destination, 3);
-//				if (size == 3) break;
-//			}
+			for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 10, 10); // cyan = waiting for button
+			while(!button_is_pressed()) {} // press button to start receiving
+			for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 0, 10); // blue = receiving
+			ReceiveFloatFromComputer((BaseSequentialStream *) &SD3, destination, 3);
 			first_start = false; null_speed_counter = 0; clear_leds();
 			GPTD12.tim->CNT = 0; // time correction for odometry
 		}
@@ -54,10 +54,11 @@ static THD_FUNCTION(Odometry, arg) {
     	time = chVTGetSystemTime();
     	if (VL53L0X_get_dist_mm() < TOF_DIST) {
     		left_motor_set_speed(0); right_motor_set_speed(0);
-    		for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 10, 10); // cyan
+//    		for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 10, 0, 10); // purple
+    		set_front_led(1);
     		GPTD12.tim->CNT = 0;
     		continue;
-    	} else for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 0, 0);
+    	} else set_front_led(0); //for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 0, 0);
     	chSysLock(); // time-sensitive calculations
     	uint16_t timestep = GPTD12.tim->CNT; // /!\ max time of ~650ms
 		if (speeds[0] != 0 && speeds[1] != 0) { // http://faculty.salina.k-state.edu/tim/robotics_sg/Control/kinematics/odometry.html
@@ -65,8 +66,8 @@ static THD_FUNCTION(Odometry, arg) {
 			dright = MM_PER_STEP*speeds[1]*timestep*1e-5;
 			dcenter = 0.5*(dleft+dright);
 			phi = INV_ROBOT_WIDTH*(dright-dleft);
-			position[X_] += dcenter*cos(position[T_]+0.5*phi);
-			position[Y_] += dcenter*sin(position[T_]+0.5*phi);
+			position[X_] += dcenter*cos(position[T_]); //+0.5*phi);
+			position[Y_] += dcenter*sin(position[T_]); //+0.5*phi);
 			position[T_] += phi;
 		}
 		chSysUnlock();
