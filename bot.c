a54b0fe2e0dd56fe70dfa7fc7b8ff35a26513c38
Y99dr451l <matthias@kockisch.eu>
2022-05-31 01:20:20 +0200

    added imu

--- bot.c
+++ bot.c
@@ -8,12 +8,19 @@
 #include "button.h"
 #include "leds.h"
 #include "sensors/VL53L0X/VL53L0X.h"
+#include "sensors/imu.h"
 #include "main.h"
 #include "bot.h"
 #include "communications.h"
 #include "pi_regulator.h"
 
+#define USE_IMU
+#define USE_TOF
+
 #define TOF_DIST 100 // round-trip, so ~50mm in real distance
+#define STANDARD_GRAVITY 9.80665f
+#define ZACC_LIMIT 7 // m/s²
+#define ZACC_CONVERSION (4*STANDARD_GRAVITY/65535) // converts raw acc value to m/s²
 
 static float position[3] = {0., 0., 0.}; // mm, mm, rad
 static float destination[3] = {0., 0., 0.}; // mm, mm, rad
@@ -25,6 +32,9 @@ static THD_FUNCTION(RunBot, arg) { // higher priority and empty while-loops for 
     chRegSetThreadName(__FUNCTION__); (void)arg;
     bool first_start = true;
     systime_t time;
+    float zacc = 0.;
+    messagebus_topic_t *imu_topic = messagebus_find_topic_blocking(&bus, "/imu");
+    imu_msg_t imu_values;
     while(1) {
     	time = chVTGetSystemTime();
 		if(button_is_pressed() || first_start) {
@@ -32,12 +42,23 @@ static THD_FUNCTION(RunBot, arg) { // higher priority and empty while-loops for 
 			clear_leds(); set_body_led(0); set_front_led(0);
 			while(button_is_pressed()) {} // in case of activation by button press, wait until release
 			for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 0, 10, 10); // cyan = waiting for button
-			while(!button_is_pressed()) {} // press and release button (again) to start receiving
-			while(button_is_pressed()) {} // make sure to have started the byte-flow with python
+			while(!button_is_pressed()) {} while(button_is_pressed()) {} // press and release to start receiving
 			ReceiveFloatFromComputer((BaseSequentialStream *) &SD3, destination, 3);
 			first_start = false; clear_leds();
 			GPTD12.tim->CNT = 0; // time correction for odometry
 		}
+#ifdef USE_IMU
+		else {
+			messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
+			zacc = (get_acc(2)-get_acc_offset(2))*ZACC_CONVERSION;
+			if (zacc > ZACC_LIMIT) {
+				left_motor_set_speed(0); right_motor_set_speed(0);
+				clear_leds(); set_body_led(0); set_front_led(0);
+				for (uint8_t i = 0; i < 4; i++) set_rgb_led(i, 10, 10, 0); // yellow = bot lifted
+				while(!button_is_pressed()) {} while(button_is_pressed()) {}
+			}
+		}
+#endif
 		set_body_led(1);
 		chThdSleepUntilWindowed(time, time + MS2ST(100)); // 10Hz
     }
@@ -50,6 +71,7 @@ static THD_FUNCTION(Odometry, arg) {
     float dleft, dright, dcenter, phi;
     while(1) {
     	time = chVTGetSystemTime();
+#ifdef USE_TOF
     	if (VL53L0X_get_dist_mm() < TOF_DIST) {
     		left_motor_set_speed(0); right_motor_set_speed(0);
     		set_front_led(1);
@@ -57,8 +79,8 @@ static THD_FUNCTION(Odometry, arg) {
     		GPTD12.tim->CNT = 0;
     		continue;
     	}
-    	set_front_led(0);
-    	obstacle = false;// remove effects from obstacle detection
+    	set_front_led(0); obstacle = false;// remove effects from obstacle detection
+#endif
     	speeds = get_speeds();
     	chSysLock(); // time-sensitive calculations
     	uint16_t timestep = GPTD12.tim->CNT; // /!\ max time of ~650ms
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ch.h"
+//#include "chmtx.h"
+//#include "chcond.h"
 #include "hal.h"
 #include "memory_protection.h"
 #include "usbcfg.h"
@@ -12,22 +14,28 @@
 #include "sensors/proximity.h"
 #include "sensors/VL53L0X/VL53L0X.h"
 #include "audio/microphone.h"
+#include "sensors/imu.h"
+#include "sensors/mpu9250.h"
 #include "main.h"
 #include "communications.h"
 #include "bot.h"
 #include "pi_regulator.h"
 #include "kalman.h"
 
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
 static void serial_start(void) {
 	static SerialConfig ser_cfg = {115200, 0, 0, 0};
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-static void timer11_start(void){
-    static const GPTConfig gpt11cfg = {10000, NULL, 0, 0}; // 10KHz timer clock to measure 100us
-    gptStart(&GPTD11, &gpt11cfg);
-    gptStartContinuous(&GPTD11, 0xFFFF);
-}
+//static void timer11_start(void){
+//    static const GPTConfig gpt11cfg = {10000, NULL, 0, 0}; // 10KHz timer clock to measure 100us
+//    gptStart(&GPTD11, &gpt11cfg);
+//    gptStartContinuous(&GPTD11, 0xFFFF);
+//}
 
 static void timer12_start(void){
     static const GPTConfig gpt12cfg = {100000, NULL, 0, 0}; // 100KHz timer clock to measure 10us
@@ -39,13 +47,17 @@ int main(void) { // order from src/main.c
 	halInit();
 	chSysInit();
 	mpu_init();
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
 	usb_start();
 	motors_init();
+	imu_start();
 	spi_comm_start();
 	VL53L0X_start();
 	serial_start();
 	mic_start(&audio_processing);
-	timer11_start(); timer12_start();
+//	timer11_start();
+	timer12_start();
+	calibrate_acc();
 	clear_leds(); set_body_led(0); set_front_led(0);
 	bot_start();
 	pi_regulator_start();
