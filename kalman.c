659a2e931e4d9e3988bc2d281d6266b9d702efee
Y99dr451l <matthias@kockisch.eu>
2022-05-30 19:24:59 +0200

    higher kalman frequency by splitting from audio-processing

--- kalman.c
+++ kalman.c
@@ -13,7 +13,7 @@
 #define DEG2RAD (M_PI/180.)
 #define FFT_SIZE 1024
 #define NB_PARAM 3
-#define BATCH_SIZE 20
+#define BATCH_SIZE 10
 #define PHYSIC_FACTOR_RL 1.4353 // speed_of_sound/(distance_between_mics*2*pi*frequency)
 #define PHYSIC_FACTOR_BF 1.3647 // same
 #define INTENSITY_TRESHOLD 300
@@ -35,13 +35,12 @@
 #define LU_TABLE_DIST_OFFSET 8
 #define FREQIND 86 // 650Hz
 
-static BSEMAPHORE_DECL(samples_ready_sem, TRUE);
 static int16_t intensity_to_distance_arr[4][8] = {{30000, 7800, 4600, 3000, 1600, 1200, 900, 700}, //order : BLFR
 												  {30000, 10000, 5000, 3000, 1700, 1300, 1000, 900},
 												  {30000, 7600, 3740, 2820, 2430, 1900, 1680, 1380},
 												  {30000, 8900, 5100, 3000, 1600, 1100, 900, 800}};
-static float32_t mag_freq_index_bff[NB_MIC];
-static float32_t mag_freq_index[NB_MIC];
+static float32_t mag_freq_index_bff[NB_MIC] = { 0 };
+static float32_t mag_freq_index[NB_MIC] = { 0 };
 static float micLeft_cmplx_input[2 * FFT_SIZE];
 static float micRight_cmplx_input[2 * FFT_SIZE];
 static float micFront_cmplx_input[2 * FFT_SIZE];
@@ -52,12 +51,13 @@ static int cntr1 = 0;
 static int cntr2 = 0;
 static float RmL = 0.;
 static float BmF = 0.;
-static float alpha_origin;
-static float x_origin;
-static float y_origin;
-static float alpha_final;
+static float alpha_origin = 0.;
+static float x_origin = 0.;
+static float y_origin = 0.;
+static float alpha_final = 0.;
 float32_t old_pos_arr[NB_PARAM] = { 0 }; //initial position at origin
-static bool CALIBRATION = true;
+static bool calibration = true;
+static bool audio_ready = false;
 
 void doFFT_optimized(float* complex_buffer) {arm_cfft_f32(&arm_cfft_sR_f32_len1024, complex_buffer, 0, 1);}
 
@@ -90,10 +90,8 @@ void audio_processing(int16_t *data, uint16_t num_samples) {
 	}
 	if (filled >= FFT_SIZE) {
 		filled = 0;
-		doFFT_optimized(micRight_cmplx_input);
-		doFFT_optimized(micLeft_cmplx_input);
-		doFFT_optimized(micBack_cmplx_input);
-		doFFT_optimized(micFront_cmplx_input);
+		doFFT_optimized(micRight_cmplx_input); doFFT_optimized(micLeft_cmplx_input);
+		doFFT_optimized(micBack_cmplx_input);  doFFT_optimized(micFront_cmplx_input);
 		float tmp1 = phase_diff(micRight_cmplx_input[FREQIND], micRight_cmplx_input[FREQIND+1], micLeft_cmplx_input[FREQIND], micLeft_cmplx_input[FREQIND+1]);
 		float tmp2 = phase_diff(micBack_cmplx_input[FREQIND], micBack_cmplx_input[FREQIND+1], micFront_cmplx_input[FREQIND], micFront_cmplx_input[FREQIND+1]);
 		if (tmp1 * tmp1 < RL_MAX) {RmL += tmp1; ++cntr1;} // noisy samples are discarded so they don't break inverse trig functions
@@ -105,8 +103,8 @@ void audio_processing(int16_t *data, uint16_t num_samples) {
 			mag_freq_index_bff[L_] = sqrt(micLeft_cmplx_input[FREQIND]  * micLeft_cmplx_input[FREQIND]  - micLeft_cmplx_input[FREQIND+1]  * micLeft_cmplx_input[FREQIND+1]);
 			mag_freq_index_bff[F_] = sqrt(micFront_cmplx_input[FREQIND] * micFront_cmplx_input[FREQIND] - micFront_cmplx_input[FREQIND+1] * micFront_cmplx_input[FREQIND+1]);
 			mag_freq_index_bff[R_] = sqrt(micRight_cmplx_input[FREQIND] * micRight_cmplx_input[FREQIND] - micRight_cmplx_input[FREQIND+1] * micRight_cmplx_input[FREQIND+1]);
-			chBSemSignal(&samples_ready_sem);
-
+			passes = 0;
+			audio_ready = true;
 		}
 	}
 }
@@ -116,6 +114,7 @@ static THD_FUNCTION(Kalman, arg) {
 	chRegSetThreadName(__FUNCTION__); (void)arg;
 	float int_from_activ_mic = 0, distance = 0;
 	uint8_t min_alpha_ind = 0;
+	systime_t time;
 	arm_matrix_instance_f32 old_pos; //vector position
 	arm_matrix_instance_f32 old_pos_gauss;
 	float32_t old_pos_gauss_arr[NB_PARAM*NB_PARAM] = { 0 };
@@ -133,7 +132,7 @@ static THD_FUNCTION(Kalman, arg) {
 	arm_mat_init_f32(&old_pos_gauss, NB_PARAM, NB_PARAM, old_pos_gauss_arr);
 	arm_mat_init_f32(&sonar_pos, NB_PARAM, 1, sonar_pos_arr);
 	{
-		float32_t rk_diag[3] = {1000000., 1000000., 1000000.};
+		float32_t rk_diag[3] = {1.e100, 1.e100, 1.e100};
 		arr_set_to_diagonal(sonar_pos_gauss_arr, rk_diag, NB_PARAM);
 	}
 	arm_mat_init_f32(&sonar_pos_gauss, NB_PARAM, NB_PARAM, sonar_pos_gauss_arr);
@@ -146,50 +145,52 @@ static THD_FUNCTION(Kalman, arg) {
 	arm_mat_init_f32(&gaussian_qk, NB_PARAM, NB_PARAM, gaussian_qk_arr);
 	GPTD11.tim->CNT = 0;
 	while (1) {
-		chBSemWait(&samples_ready_sem);
-		for (int i = 0 ; i < NB_MIC ; i++) mag_freq_index[i] = mag_freq_index_bff[i]; // double buffering
-		{
-			float alpha_rl, alpha_bf;
-			passes = 0;
-			alpha_rl = asin(PHYSIC_FACTOR_RL*RmL/cntr1); // angles between source and 2 pairs of microphones
-			alpha_bf = asin(PHYSIC_FACTOR_BF*BmF/cntr2);
-			cntr1 = 0; cntr2 = 0; RmL = 0; BmF = 0;
-			// slope & offset are chosen by experience with a source going around the device, to optimally fit reality
-			if (alpha_rl > 0) {
-				if (alpha_bf > -10) alpha_final = PP_OFFSET + alpha_rl * PP_SLOPE;
-				else alpha_final = PN_OFFSET - alpha_rl * PN_SLOPE;
-			} else {
-				if (alpha_bf > 0) alpha_final = NP_OFFSET + alpha_rl * NP_SLOPE;
-				else if (alpha_rl < -20) alpha_final = NN_OFFSET - alpha_bf * NN_SLOPE;
-				else alpha_final = NN_OFFSET2 - alpha_bf * NN_SLOPE2;
-			}
-		}
-		//distance to source calculation :
-		{ //find microphone closest to the source
-			float32_t min_alpha_arr[NB_MIC + 1] = {0., 0., 0., 0., 0.}; //4 microphones, but we must check +pi and -pi which both count for the back microphone
-			//compare alpha_final, to the angle associated to each microphone (back_m = -pi/+pi, left_m = +pi/2, right_m = -pi/2, front_m = 0
-			for (int i = 0; i < NB_MIC + 1 ; i++) min_alpha_arr[i] = alpha_final + (-M_PI) + 2 * M_PI/NB_MIC * i;
-			float min_alpha = 2 * M_PI; //maximum value obtainable
-			//find microphone that minimizes it's angle to alpha_final
-			for (int i = 0; i < NB_MIC + 1 ; i++)
-				if (min_alpha_arr[i] * min_alpha_arr[i] < min_alpha * min_alpha) {
-					min_alpha = min_alpha_arr[i];
-					min_alpha_ind = i;
-					int_from_activ_mic = mag_freq_index[i];
+		time = chVTGetSystemTime();
+		if(audio_ready) {
+			audio_ready = false;
+			for (int i = 0 ; i < NB_MIC ; i++) mag_freq_index[i] = mag_freq_index_bff[i]; // double buffering
+			{
+				float alpha_rl = 0., alpha_bf = 0.;
+				if (cntr1 != 0) alpha_rl = asin(PHYSIC_FACTOR_RL*RmL/cntr1); // angles between source and 2 pairs of microphones
+				if (cntr2 != 0) alpha_bf = asin(PHYSIC_FACTOR_BF*BmF/cntr2);
+				cntr1 = 0; cntr2 = 0; RmL = 0; BmF = 0;
+				// slope & offset are chosen by experience with a source going around the device, to optimally fit reality
+				if (alpha_rl > 0) {
+					if (alpha_bf > -10) alpha_final = PP_OFFSET + alpha_rl * PP_SLOPE;
+					else alpha_final = PN_OFFSET - alpha_rl * PN_SLOPE;
+				} else {
+					if (alpha_bf > 0) alpha_final = NP_OFFSET + alpha_rl * NP_SLOPE;
+					else if (alpha_rl < -20) alpha_final = NN_OFFSET - alpha_bf * NN_SLOPE;
+					else alpha_final = NN_OFFSET2 - alpha_bf * NN_SLOPE2;
 				}
-		}
-		//evaluate distance by framing measured intensity with 2 values from reference table
-		for (int i = 0 ; i < LU_TABLE_NB_VALUES - 1 ; i++) {
-			if ((int_from_activ_mic < intensity_to_distance_arr[min_alpha_ind][i]) && (int_from_activ_mic > intensity_to_distance_arr[min_alpha_ind][i+1])) {
-				//fixed path between each table element is 55mm with a starting offset of 8mm, obtained by experiencing
-				distance = i * LU_TABLE_DIST_STEP + LU_TABLE_DIST_OFFSET; break;
 			}
-		}
-		if (CALIBRATION) {
-			alpha_origin = alpha_final;
-			x_origin = distance * cos(alpha_final);
-			y_origin = distance * sin(alpha_final);
-			CALIBRATION = false; continue;
+			//distance to source calculation :
+			{ //find microphone closest to the source
+				float32_t min_alpha_arr[NB_MIC + 1] = {0., 0., 0., 0., 0.}; //4 microphones, but we must check +pi and -pi which both count for the back microphone
+				//compare alpha_final, to the angle associated to each microphone (back_m = -pi/+pi, left_m = +pi/2, right_m = -pi/2, front_m = 0
+				for (int i = 0; i < NB_MIC + 1 ; i++) min_alpha_arr[i] = alpha_final + (-M_PI) + 2 * M_PI/NB_MIC * i;
+				float min_alpha = 2 * M_PI; //maximum value obtainable
+				//find microphone that minimizes it's angle to alpha_final
+				for (int i = 0; i < NB_MIC + 1 ; i++)
+					if (min_alpha_arr[i] * min_alpha_arr[i] < min_alpha * min_alpha) {
+						min_alpha = min_alpha_arr[i];
+						min_alpha_ind = i;
+						int_from_activ_mic = mag_freq_index[i];
+					}
+			}
+			//evaluate distance by framing measured intensity with 2 values from reference table
+			for (int i = 0 ; i < LU_TABLE_NB_VALUES - 1 ; i++) {
+				if ((int_from_activ_mic < intensity_to_distance_arr[min_alpha_ind][i]) && (int_from_activ_mic > intensity_to_distance_arr[min_alpha_ind][i+1])) {
+					//fixed path between each table element is 55mm with a starting offset of 8mm, obtained by experiencing
+					distance = i * LU_TABLE_DIST_STEP + LU_TABLE_DIST_OFFSET; break;
+				}
+			}
+			if (calibration) {
+				alpha_origin = alpha_final;
+				x_origin = distance * cos(alpha_final);
+				y_origin = distance * sin(alpha_final);
+				calibration = false; continue;
+			}
 		}
 		//Start Kalman
 		{ //update from sonar data
@@ -210,7 +211,7 @@ static THD_FUNCTION(Kalman, arg) {
 			float32_t tmp_mat_arr[NB_PARAM * 1], tmp_mat2_arr[NB_PARAM * 1];
 			arm_mat_init_f32(&tmp_mat, NB_PARAM, 1, tmp_mat_arr);
 			arm_mat_init_f32(&tmp_mat2, NB_PARAM, 1, tmp_mat2_arr);
-			arm_mat_scale_f32(&control_mat, (GPTD11.tim->CNT)*1e-4, &tmp_mat); //B*u
+			arm_mat_scale_f32(&control_mat, (GPTD11.tim->CNT)*1e-4, &tmp_mat); //B*u, timestep is in 100us
 			GPTD11.tim->CNT = 0;
 			arm_mat_add_f32(&tmp_mat, &old_pos, &tmp_mat2);	//x = FxF^T + B*u; F = I
 			memcpy(old_pos_arr, tmp_mat2_arr, sizeof(old_pos_arr));
@@ -219,7 +220,7 @@ static THD_FUNCTION(Kalman, arg) {
 			arm_matrix_instance_f32 tmp_mat;
 			float32_t tmp_mat_arr[NB_PARAM * NB_PARAM];
 			arm_mat_init_f32(&tmp_mat, NB_PARAM, NB_PARAM, tmp_mat_arr);
-			arm_mat_add_f32(&old_pos_gauss, &gaussian_qk, &tmp_mat);	//P = FPF^T + Q_k; F = I
+			arm_mat_add_f32(&old_pos_gauss, &gaussian_qk, &tmp_mat); //P = FPF^T + Q_k; F = I
 			memcpy(old_pos_gauss_arr, tmp_mat_arr, sizeof(old_pos_gauss_arr));
 		}
 		{//Kalman Gain calculation:
@@ -251,6 +252,7 @@ static THD_FUNCTION(Kalman, arg) {
 			arm_mat_sub_f32(&old_pos_gauss, &tmp_mat, &tmp_mat2); //P = P - K*P
 			memcpy(old_pos_gauss_arr, tmp_mat2_arr, sizeof(old_pos_gauss_arr));
 		}
+		chThdSleepUntilWindowed(time, time + MS2ST(20)); // 50Hz
 	}
 }
 
